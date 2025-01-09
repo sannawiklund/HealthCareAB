f0ab12152cc65bc0915693821ba6f30e802d5ed2
msgregorsamsa <142659843+msgregorsamsa@users.noreply.github.com>
2025-01-09 15:03:54 +0100

    Feat: Add controller and DTO for booking appointments.
    This is the initial draft, I have not been able to test it fully as we do not have available times as of yet,

---
+++ HealthCareABApi/DTO/AppointmentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthCareABApi.DTO
+{
+    public class AppointmentDTO
+    {
+
+        public string CaregiverId { get; set; }
+        public DateTime AppointmentTime { get; set; }
+    }
+}

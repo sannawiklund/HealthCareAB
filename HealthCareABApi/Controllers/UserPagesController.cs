3fa0f69214ec90a1e336cf0ec722f8645a51e5e5
msgregorsamsa <142659843+msgregorsamsa@users.noreply.github.com>
2025-01-13 12:08:57 +0100

    Feat: GET api endpoint for user information, initial commit.
    Created a userinterface and userrepository to get information about a user.  Made the interface so that more functionality can be built directly from it and then used in the UserPageController.

---
+++ HealthCareABApi/Controllers/UserPagesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HealthCareABApi.DTO;
+using HealthCareABApi.Repositories;
+
+namespace HealthCareABApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserPagesController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserPagesController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+
+    }
---
+++ HealthCareABApi/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using HealthCareABApi.Models;
+
+namespace HealthCareABApi.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByIdAsync(string id);
+        Task<User>GetByUsernameAsync(string username);
+        Task<List<User>> GetAllAsync();
+        Task AddAsync(User user);
+        Task UpdateAsync(string id, User user);
+        Task DeleteAsync(string id);
+    }
+}

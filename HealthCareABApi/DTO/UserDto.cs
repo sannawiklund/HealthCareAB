325923cf3f0db2d155a65e6c26cfed9e9d568582
Sanna Wiklund <sanna@webmind.se>
2025-01-14 11:32:44 +0100

    Re-added roles to UserDTO after failed test

--- HealthCareABApi/DTO/UserDto.cs
+++ HealthCareABApi/DTO/UserDto.cs
@@ -2,6 +2,8 @@
 {
     public class UserDto
     {
+        public List<string> Roles { get; set; }
+
         public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }

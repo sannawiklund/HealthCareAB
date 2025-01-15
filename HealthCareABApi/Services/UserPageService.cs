3d0b676478941f4e189730298f3dcb54076eee54
Anna <142659843+msgregorsamsa@users.noreply.github.com>
2025-01-15 15:21:06 +0100

    31 personal profile implement a post method that enables the user to update or change data (#60)
    * Feat(WIP): Implement Put-api call to be able to change
    
    To fix: Make sure the changes dont leave an empty string or the word string if the user does not change anything.
    Solve the problem with using an API that presents the role, as the user should not be able to see this field, and def not be able to change it as they want :P
    
    * feat: Add extended models for user and caregiver
    
    * Fix: Update naming to be more clear
    
    * feat: Changed some of the properties after PR-feedback
    
    * Re-added roles to UserDTO after failed test
    
    * Fix: Update with more variables. Update from dateOnly to DateTime that was directly compatible w json
    
    Chose to use DateTime to not have to build a custom converter - there might be time to implement something like that further along in the project.
    
    * Refactor: Update code to be separated into service and controller layers
    
    * Refactor: New service for userPage that makes use of the repository and interface
    
    Due to the mess that was using two constructors in one service I decided to create a new Service specific for UserPage.

---
+++ HealthCareABApi/Services/UserPageService.cs
@@ -0,0 +1,63 @@
+﻿using HealthCareABApi.Models;
+using HealthCareABApi.DTO;
+using HealthCareABApi.Repositories;
+
+namespace HealthCareABApi.Services
+{
+    public class UserPageService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserPageService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+
+        }
+
+        public async Task<UserDto> GetUserInformationAsync(string userId)
+        {
+            var user = await _userRepository.GetByIdAsync(userId);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            return new UserDto
+            {
+                Username = user.Username,
+                Roles = user.Roles,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Phone = user.Phone,
+                Address = user.Address,
+                Gender = user.Gender,
+                DateOfBirth = user.DateOfBirth
+            };
+        }
+
+        public async Task<bool> UpdateUserInformationAsync(string userId, UserDto userDto)
+        {
+            var user = await _userRepository.GetByIdAsync(userId);
+
+            if (user == null)
+            {
+                return false;
+            }
+
+            //Uppdaterar användarens information (förutsatt att de ändrat något, annars behålls gamla värdet)
+            if (!string.IsNullOrEmpty(userDto.Username)) user.Username = userDto.Username;
+            if (!string.IsNullOrEmpty(userDto.FirstName)) user.FirstName = userDto.FirstName;
+            if (!string.IsNullOrEmpty(userDto.LastName)) user.LastName = userDto.LastName;
+            if (!string.IsNullOrEmpty(userDto.Email)) user.Email = userDto.Email;
+            if (!string.IsNullOrEmpty(userDto.Phone)) user.Phone = userDto.Phone;
+            if (!string.IsNullOrEmpty(userDto.Address)) user.Address = userDto.Address;
+
+            await _userRepository.UpdateAsync(userId, user);
+
+            return true;
+        }
+
+    }
+}
